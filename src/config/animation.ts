72ccbc43442499d6957e9042ba6b7cad89e2c2fc
moneyinto <woyaoqianjin@gmail.com>
2023-05-22 13:56:13 +0800

    配置 及 README

---
+++ src/config/animation.ts
@@ -0,0 +1,350 @@
+export const ANIMATION_LIST = [
+    {
+        name: "Attention seekers",
+        children: [
+            {
+                name: "bounce"
+            },
+            {
+                name: "flash"
+            },
+            {
+                name: "pulse"
+            },
+            {
+                name: "rubberBand"
+            },
+            {
+                name: "shakeX"
+            },
+            {
+                name: "shakeY"
+            },
+            {
+                name: "headShake"
+            },
+            {
+                name: "swing"
+            },
+            {
+                name: "tada"
+            },
+            {
+                name: "wobble"
+            },
+            {
+                name: "jello"
+            },
+            {
+                name: "heartBeat"
+            }
+        ]
+    },
+    {
+        name: "Back entrances",
+        children: [
+            {
+                name: "backInDown"
+            },
+            {
+                name: "backInLeft"
+            },
+            {
+                name: "backInRight"
+            },
+            {
+                name: "backInUp"
+            }
+        ]
+    },
+    {
+        name: "Back exits",
+        children: [
+            {
+                name: "backOutDown"
+            },
+            {
+                name: "backOutLeft"
+            },
+            {
+                name: "backOutRight"
+            },
+            {
+                name: "backOutUp"
+            }
+        ]
+    },
+    {
+        name: "Bouncing entrances",
+        children: [
+            {
+                name: "bounceIn"
+            },
+            {
+                name: "bounceInDown"
+            },
+            {
+                name: "bounceInLeft"
+            },
+            {
+                name: "bounceInRight"
+            },
+            {
+                name: "bounceInUp"
+            }
+        ]
+    },
+    {
+        name: "Bouncing exits",
+        children: [
+            {
+                name: "bounceOut"
+            },
+            {
+                name: "bounceOutDown"
+            },
+            {
+                name: "bounceOutLeft"
+            },
+            {
+                name: "bounceOutRight"
+            },
+            {
+                name: "bounceOutUp"
+            }
+        ]
+    },
+    {
+        name: "Fading entrances",
+        children: [
+            {
+                name: "fadeIn"
+            },
+            {
+                name: "fadeInDown"
+            },
+            {
+                name: "fadeInDownBig"
+            },
+            {
+                name: "fadeInLeft"
+            },
+            {
+                name: "fadeInLeftBig"
+            },
+            {
+                name: "fadeInRight"
+            },
+            {
+                name: "fadeInRightBig"
+            },
+            {
+                name: "fadeInUp"
+            },
+            {
+                name: "fadeInUpBig"
+            }
+        ]
+    },
+    {
+        name: "Fading exits",
+        children: [
+            {
+                name: "fadeOut"
+            },
+            {
+                name: "fadeOutDown"
+            },
+            {
+                name: "fadeOutDownBig"
+            },
+            {
+                name: "fadeOutLeft"
+            },
+            {
+                name: "fadeOutLeftBig"
+            },
+            {
+                name: "fadeOutRight"
+            },
+            {
+                name: "fadeOutRightBig"
+            },
+            {
+                name: "fadeOutUp"
+            },
+            {
+                name: "fadeOutUpBig"
+            }
+        ]
+    },
+    {
+        name: "Flippers",
+        children: [
+            {
+                name: "flip"
+            },
+            {
+                name: "flipInX"
+            },
+            {
+                name: "flipInY"
+            },
+            {
+                name: "flipOutX"
+            },
+            {
+                name: "flipOutY"
+            }
+        ]
+    },
+    {
+        name: "Lightspeed",
+        children: [
+            {
+                name: "lightSpeedInRight"
+            },
+            {
+                name: "lightSpeedInLeft"
+            },
+            {
+                name: "lightSpeedOutRight"
+            },
+            {
+                name: "lightSpeedOutLeft"
+            }
+        ]
+    },
+    {
+        name: "Rotating entrances",
+        children: [
+            {
+                name: "rotateIn"
+            },
+            {
+                name: "rotateInDownLeft"
+            },
+            {
+                name: "rotateInDownRight"
+            },
+            {
+                name: "rotateInUpLeft"
+            },
+            {
+                name: "rotateInUpRight"
+            }
+        ]
+    },
+    {
+        name: "Rotating exits",
+        children: [
+            {
+                name: "rotateOut"
+            },
+            {
+                name: "rotateOutDownLeft"
+            },
+            {
+                name: "rotateOutDownRight"
+            },
+            {
+                name: "rotateOutUpLeft"
+            },
+            {
+                name: "rotateOutUpRight"
+            }
+        ]
+    },
+    {
+        name: "Specials",
+        children: [
+            {
+                name: "hinge"
+            },
+            {
+                name: "jackInTheBox"
+            },
+            {
+                name: "rollIn"
+            },
+            {
+                name: "rollOut"
+            }
+        ]
+    },
+    {
+        name: "Zooming entrances",
+        children: [
+            {
+                name: "zoomIn"
+            },
+            {
+                name: "zoomInDown"
+            },
+            {
+                name: "zoomInLeft"
+            },
+            {
+                name: "zoomInRight"
+            },
+            {
+                name: "zoomInUp"
+            }
+        ]
+    },
+    {
+        name: "Zooming exits",
+        children: [
+            {
+                name: "zoomOut"
+            },
+            {
+                name: "zoomOutDown"
+            },
+            {
+                name: "zoomOutLeft"
+            },
+            {
+                name: "zoomOutRight"
+            },
+            {
+                name: "zoomOutUp"
+            }
+        ]
+    },
+    {
+        name: "Sliding entrances",
+        children: [
+            {
+                name: "slideInUp"
+            },
+            {
+                name: "slideInDown"
+            },
+            {
+                name: "slideInLeft"
+            },
+            {
+                name: "slideInRight"
+            }
+        ]
+    },
+    {
+
+        name: "Sliding exits",
+        children: [
+            {
+                name: "slideOutUp"
+            },
+            {
+                name: "slideOutDown"
+            },
+            {
+                name: "slideOutLeft"
+            },
+            {
+                name: "slideOutRight"
+            }
+        ]
+    }
+];
--- src/layout/animationList.vue
+++ src/layout/animationList.vue
@@ -1,13 +1,52 @@
 <template>
     <div class="animation-list">
-        xxx
+        <div
+            class="animation-header"
+            v-for="item in ANIMATION_LIST"
+            :key="item.name"
+        >
+            <h2 class="animation-title">{{ item.name }}</h2>
+            <div
+                class="animation-item"
+                v-for="child in item.children"
+                :key="child.name"
+                @click="selectAnimation(child.name)"
+            >
+                {{ child.name }}
+            </div>
+        </div>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ANIMATION_LIST } from "@/config/animation";
+
+const selectAnimation = (animation: string) => {
+    console.log(animation);
+};
+</script>
 
 <style>
 .animation-list {
     padding: 25px 15px;
+    height: 100%;
+    overflow-y: auto;
+    user-select: none;
+}
+
+.animation-header {
+    margin-bottom: 30px;
+}
+
+.animation-title {
+    font-weight: 600;
+    margin-bottom: 5px;
+}
+
+.animation-item {
+    cursor: pointer;
+    line-height: 1.4;
+    font-size: 18px;
+    padding-left: 5px;
 }
 </style>
